d5416e847c0d7b30e51e0db664909a9a1966c20d
rajbrady <raj@alan.app>
2020-10-18 05:27:48 -0400

    DONE

---
+++ GTHacks/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  GTHacks
+//
+//  Created by Raj Janardhan on 10/17/20.
+//  Copyright © 2020 Shubhankar Baliyan. All rights reserved.
+//
+
+import Foundation
---
+++ GTHacks/ShowLink.swift
@@ -0,0 +1,30 @@
+//
+//  ShowLink.swift
+//  GTHacks
+//
+//  Created by Raj Janardhan on 10/18/20.
+//  Copyright © 2020 Shubhankar Baliyan. All rights reserved.
+//
+
+import UIKit
+
+class ShowLink: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
